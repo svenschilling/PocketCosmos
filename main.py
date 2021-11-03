1426b054dd4295c53b09900de67f4a6c8653c26a
svenschilling <sven.schilling@gmail.com>
2021-11-03 17:02:13 +0100

    first upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#           POCKETCOSMOS
+import kivy
+kivy.require('2.0.0')
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+class PocketCosmosFloatLayout(FloatLayout):
+    def button_pressed(self):
+        print("you clicked it")
+    pass
+
+class PocketCosmosApp(App):
+    def build(self):
+        return PocketCosmosFloatLayout()
+    
+
+PocketCosmosApp().run()
